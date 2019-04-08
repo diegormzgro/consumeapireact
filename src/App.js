129408e76a1a1adbdc4bbceef96a8cde56cc933e
Diego Ramirez <diegormzgro@gmail.com>
2019-04-08 05:54:20 -0500

    tarea4react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      loading: false,
+      cards:[],
+      url: 'http://daw-tcg.test/api/cards'
+    }
+  }
+
+  componentDidMount()
+  {
+    this.getCard();
+  }
+
+  getCard = () => {
+    this.setState({loading: true})
+
+    fetch(this.state.url)
+    .then(res => res.json())
+    .then(res => {
+
+      this.setState({
+        cards: res.data,
+        loading: false
+      })
+
+    });
+  };
   render() {
+    
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      
+        
+      
+
+      
+        <div>
+           
+           {this.state.cards.map(card => (
+
+             <li>
+               Name: {card.name} | attack: {card.attack} | defense: {card.defense}
+             </li>
+           )   
+            
+            
+            )}
+         
+
+        </div>
+      
+      
     );
   }
 }
